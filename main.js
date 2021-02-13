81a90478cb7c8eb703f615939eb1cb4be092a85b
Niranjana4 <75379681+Niranjana4@users.noreply.github.com>
2021-02-13 15:28:56 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+nosex=0;
+nosey=0;
+difference=0;
+rightwristx=0;
+lieftwristx=0;
+
 function setup(){
     video=createCapture(VIDEO);
     video.size(550,500);
@@ -8,7 +14,12 @@ function setup(){
 }
 
 function draw(){
-    background('#969A97');
+    background('#000000');
+    document.getElementById("name").innerHTML="width and height of the text wil be"+difference+"px";
+    fill('#ADD8E6');
+    stroke('#ADD8E6');
+    textSize(difference,10,10);
+    text("Niranjana",10, 100);
 }
 
 function model_loaded(){
@@ -18,5 +29,12 @@ function model_loaded(){
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
+        nosex=results[0].pose.nose.x;
+        nosey=results[0].pose.nose.y;
+        console.log(nosex,nosey);
+        leftwristx=results[0].pose.leftWrist.x;
+        rightwristx=results=results[0].pose.rightWrist.x;
+        difference=floor(leftwristx-rightwristx);
+        console.log(leftwristx,rightwristx);
     }
 }
